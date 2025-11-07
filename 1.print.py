8792a68f2f734f9bdbb7816c0d315acf829b0f8c
madnanch786 <madnanch2016@gmail.com>
2025-11-06 17:53:38 -0800

    ADDED: Print Practice Code file Added

---
+++ 1.print.py
@@ -0,0 +1,17 @@
+# Print a string
+print("Here is a string")
+# print a single variable
+a=25
+print(a)
+# print an expresion
+a=25
+b=9.20
+print(a+b)
+# print multiple variables
+name="Adnan"
+rollno=25225
+print("name: ",name,"Roll No: ", rollno)
+# add new line
+name="Adnan"
+rollno=25225
+print("name: ",name,"\nRoll No: ", rollno)
