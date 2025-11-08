94ddbc60d5f6489e24c76b974cd48866f41a0665
madnanch786 <madnanch2016@gmail.com>
2025-11-07 20:52:52 -0800

    ADDED : Variable and Data Types

---
+++ 2.variable_Date_Types.py
@@ -0,0 +1,32 @@
+'''
+There are different types of Data types
+1. Intege (5,6,-96)
+2. Float (8.3,8.0)
+3. String (adnan,name)
+4. Bolean (True or False)
+3. None (to store nothing in a variable)
+'''
+# Interger variable
+a=5
+print("Data Type of Variable",type(a))
+print("a=",a)
+
+# Float variable
+b=5.5
+print("Data Type of Variable",type(b))
+print("b=",b)
+
+# String variable
+name="Usman"
+print("Data Type of Variable",type(name))
+print("Name=",name)
+
+# Bolean variable
+Gender=True
+print("Data Type of Variable",type(Gender))
+print("Gender=",Gender)
+
+# None variable
+none=None
+print("Data Type of Variable",type(none))
+print("none=",none)
